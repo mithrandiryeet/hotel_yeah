17076e55b09573d6197353d949c20dc0e42e859e
mithrandiryeet <122556370+mithrandiryeet@users.noreply.github.com>
2023-04-13 11:58:00 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // hello
+
+//date and time JS
+var dt = new Date();
+document.getElementById('date-time').innerHTML=dt;
+
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+  $(function() {
+    console.log( "ready!" );
+  
+  //click the eye icon w/class reveal 
+  $(".reveal").on('click',function() {
+    let $pwd = $(".pwd");
+    
+    if ($pwd.attr('type') === 'password') {
+        $pwd.attr('type', 'text');
+       $(this).children('i').addClass('bi-eye-slash-fill').removeClass('bi-eye-fill');
+    } else {
+        $pwd.attr('type', 'password');
+     $(this).children('i').removeClass('bi-eye-slash-fill').addClass('bi-eye-fill');
+    }
+});
+  
+});
